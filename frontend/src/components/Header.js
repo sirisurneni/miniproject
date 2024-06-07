0a3765104d3095beb32ef598fb6dd2ff35206c9a
siri <surnenisiri@gmail.com>
2024-06-07 10:22:01 +0530

    first

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from './Logo'
+const Header = () => {
+  return (
+    <header className='h-16 shadow-md' >
+      <div className='h-full container mx-auto flex items-center'>
+        <div className=''>
+          <Logo w={90} h={50}/>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
