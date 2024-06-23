1ea05609be3d5e04980db0d2c24d7fc7f6860a12
siri <surnenisiri@gmail.com>
2024-06-23 11:58:32 +0530

    login

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,12 +3,15 @@ import Logo from './Logo'
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
+import {Link} from "react-router-dom"
 const Header = () => {
   return (
-    <header className='h-16 shadow-md' >
+    <header className='h-16 shadow-md bg-white' >
       <div className='h-full container mx-auto flex items-center px-4 justify-between'>
         <div className=''>
-          <Logo w={10} h={10}/>
+          <Link to={"/"}>
+          <Logo w={10} h={10}/></Link>
+          
         </div>
         <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
           <input type='text' placeholder='search here...' className='w-full outline-none '/>
@@ -30,9 +33,9 @@ const Header = () => {
           </div>
           </div>
           <div>
-            <button className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>
+            <Link to={"/login"}className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>
               Login
-            </button>
+            </Link>
           </div>
         </div>
       </div>
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,8 @@
 import {createBrowserRouter} from 'react-router-dom'
 import App from '../App'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
+import ForgotPassword from '../pages/ForgotPassword'
 const router=createBrowserRouter(
 [{
     path : "/",
@@ -9,6 +11,14 @@ const router=createBrowserRouter(
         {
             path : "",
             element : <Home/>
+        },
+        {
+            path:"login",
+            element:<Login/>
+        },
+        {
+            path:"forgot-password",
+            element : <ForgotPassword/>
         }
     ]
 
