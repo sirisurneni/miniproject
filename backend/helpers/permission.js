e3fd06d1d7dd2e32875f9544177985fa778d2284
Boora Shruthi <boorashruthi123@gmail.com>
2024-09-01 21:54:59 +0530

    7:20:23

--- backend/helpers/permission.js
+++ backend/helpers/permission.js
@@ -3,8 +3,8 @@ const userModel = require("../models/userModel")
 const uploadProductPermission = async(userId)=>{
     const user = await userModel.findById(userId)
 
-    if(user.role === 'ADMIN'){
-        return true
+    if(user.role !=='ADMIN'){
+        return false
     }
 
     return false
